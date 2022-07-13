444452e2bcbcab9feca4eeca3c85eddca5e0505b
Bohyeon Kim <bh.kim@nemolab.co.kr>
2022-07-13 15:02:19 +0900

    db 구조 변경(score, time, nickname 추가)

--- app.py
+++ app.py
@@ -59,17 +59,15 @@ def edit_page():
 def edit_review(reviewNo):
     
     book_list = list(review_db.review_test.find({"content_no":int(reviewNo)},{'_id':False}))
-
-    print(book_list)
     
     title = book_list[0]['title']
     content = book_list[0]['content']
     image_url = book_list[0]['file']
+    star_score = book_list[0]['star_score']
     
-    return render_template("edit-page.html", title=title, content=content, image=image_url, review_no=reviewNo, isEdit=title)
+    return render_template("edit-page.html", title=title, content=content, image=image_url, review_no=reviewNo, isEdit=title, star_score=star_score)
 
-
-# 네이버를 통해 검색된 책자의 리뷰를 작성
+# 네이버를 통해 검색된 책자의 리뷰를 작성 @김보현
 @app.route('/edit-page/<isbn>')
 def find_bookdetail_via_isbn(isbn):
     
@@ -88,30 +86,33 @@ def find_bookdetail_via_isbn(isbn):
     
     return render_template("edit-page.html", title=title, image=image_url, author = author, description = description)
 
-
 # 작성된 리뷰를 저장 @김보현
 @app.route('/save-review', methods=['POST'])
 def save_review():
     
     all_reviews = list(review_db.review_test.find({},{'_id':False}))
-    print(all_reviews[-1]['content_no'])
+        
+    review_count = all_reviews[-1]['content_no'] + 1
     
     booktitle = request.form['booktitle_give']
     review_content = request.form['review_content_give']
     img_url = request.form['imgfile_give']
+    writer_nickname = request.form['writer_nickname']
+    star_score = int(request.form['star_score'])
     
     doc = {
         'title': booktitle,
         'content':review_content,
         'file': img_url,
-        'time': datetime.now().strftime('%Y.%m.%d'),
-        'content_no': all_reviews[-1]['content_no']+1
+        'time': datetime.now().strftime('%Y.%m.%d.%H:%M:%S'),
+        'writer_nickname' : '둘리',
+        'star_score' : star_score,
+        'content_no': review_count
         }
 
     review_db.review_test.insert_one(doc)
     
     return render_template("index.html")
-
 
 # 수정된 리뷰를 저장 @김보현
 @app.route('/update-review', methods=['POST'])
@@ -119,18 +120,18 @@ def update_review():
     
     booktitle = request.form['booktitle_give']
     review_content = request.form['review_content_give']
-    img_url = request.form['imgfile_give']
     content_no = int(request.form['edited_content_no'])
-
+    star_score = int(request.form['star_score'])
+    
     doc = {
     'title': booktitle,
     'content':review_content,
+    'star_score' : star_score,
     }
         
     review_db.review_test.update_one({'content_no':content_no},{'$set':doc})
     
     return render_template("index.html")
-
 
 # 리뷰 삭제 @김보현
 @app.route('/delete/<reviewNo>')
@@ -139,7 +140,6 @@ def delete_review(reviewNo):
     review_db.review_test.delete_one({'content_no':int(reviewNo)})
         
     return render_template("index.html")
-    
     
 # naverapi에서 검색어 정보를 불러오는 코드 @김보현
 @app.route('/api/call-bookinfo', methods=['POST'])
@@ -154,8 +154,6 @@ def give_bookInfo():
     result = r.json()
     
     return result
-
-
 
 # 로그인페이지 @금윤성
 @app.route('/login')
